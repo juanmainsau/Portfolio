a2f0c43fa723cfdbf829f649073e395625a6061e
juanmainsau <datosjuanmainsau@gmail.com>
2025-05-20 21:43:49 -0300

    resto de cambios para migracion a tailwind
    de no funcionar tenemos un punto de recuperación, que es el antepenúltimo cambio con referencia a este.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,43 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-    content: ["./**/*.{html,js}"],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  }
-
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'dark-bg': '#333',
+        'darker-bg': '#222',
+        'light-bg': '#f4f4f4',
+        'header-bg': '#e7e7ed',
+        'text-dark': '#0f0f0a'
+      },
+      backgroundImage: {
+        'presentation-gradient': 'linear-gradient(to bottom, rgba(255,255,255,0.15) 0%, rgba(0,0,0,0.15) 100%), radial-gradient(at top center, rgba(255,255,255,0.40) 0%, rgba(0,0,0,0.40) 120%)'
+      },
+      fontFamily: {
+        prompt: ['Prompt', 'sans-serif']
+      },
+      scale: {
+        130: '1.3'
+      },
+      borderRadius: {
+        '15': '15px'
+      }
+    }
+  },
+  safelist: [
+    'bg-dark-bg',
+    'bg-darker-bg',
+    'bg-light-bg',
+    'bg-header-bg',
+    'text-white',
+    'text-dark',
+    'font-prompt',
+    'bg-presentation-gradient',
+    'hover:scale-130',
+    'rounded-15'
+  ],
+  plugins: []
+}
